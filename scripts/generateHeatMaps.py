e274042248543874214a2e917302bd3a2b459118
bertranddelvaux <bertrand.delvaux@gmail.com>
2024-11-12 11:08:18 +0000

    Added arg in generateHeatMaps.py

--- scripts/generateHeatMaps.py
+++ scripts/generateHeatMaps.py
@@ -6,8 +6,14 @@
 #   a. resolution low, high
 #   b. years span
 #      i. if None, the whole history since 1980 season
-#      ii. if only one year mentioned, the whole history since that year
+#      ii. if only one year mentioned, e.g. '1986', process only that year
+#      iii. if only one year mentioned with a plus, e.g. '1986+', process the whole history since that year
+#      iv. if a year span mentioned, e.g. '1986-2004', process the span
 #   c. type of algorithm
+#      i. max_wind_speed
+#      ii. number_of_hits
+#      iii. median_wind_speed
+#      iv. degree_of_severity
 # 3. Process
 #   a. collect a list of files to go through, given the argparse
 #   b. open the exposure dataset (depending on low or high resolution)
@@ -19,3 +25,29 @@
 #   e. write out the dataset to a geojson
 # NOTE:
 #   A. Don't forget to include the most recent years!!! (Historical data stop at 2021)
+
+
+import os
+import argparse
+
+
+def generateHeatMap(
+        res = 'high',
+
+):
+
+    exit()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Arguments to be passed to the script')
+    parser.add_argument('-r', '--resolution', type=str, default='high', dest='res', help="Set the resolution of the data: 'low' or 'high'.")
+    args = parser.parse_args()
+
+    if args.res not in ['low', 'high']:
+        raise argparse.ArgumentTypeError("Resolution must be either 'low' or 'high'.")
+
+    generateHeatMap(
+        res=args.res
+    )
