f4f557df2d38c5ddd91868f7b93454e0125d33fc
bertranddelvaux <bertrand.delvaux@gmail.com>
2024-11-12 11:48:45 +0000

    Added the arguments in generateHeatMaps.py

--- scripts/generateHeatMaps.py
+++ scripts/generateHeatMaps.py
@@ -29,11 +29,18 @@
 
 import os
 import argparse
+import datetime
+
+
+# a few constants
+HISTORICAL_START_YEAR = 1980
 
 
 def generateHeatMap(
+        start,
+        end,
+        alg,
         res = 'high',
-
 ):
 
     exit()
@@ -41,13 +48,38 @@ def generateHeatMap(
 
 if __name__ == '__main__':
 
+    current_year = datetime.datetime.now().year
+
     parser = argparse.ArgumentParser(description='Arguments to be passed to the script')
-    parser.add_argument('-r', '--resolution', type=str, default='high', dest='res', help="Set the resolution of the data: 'low' or 'high'.")
+    parser.add_argument('-res', '--resolution', type=str, default='high', dest='res', help="Set the resolution of the data: 'low' or 'high'.")
+    parser.add_argument('-start', type=int, default=HISTORICAL_START_YEAR, dest='start', help="Starting year to be processed.")
+    parser.add_argument('-end', type=int, default=current_year, dest='end', help="Ending year to be processed.")
+    parser.add_argument('-alg', type=str, required=True, dest='alg', help="Algorithm to process the data")
     args = parser.parse_args()
 
+    def year_out_of_range(year):
+        current_year = datetime.datetime.now().year
+        if year < HISTORICAL_START_YEAR or year > current_year:
+            raise argparse.ArgumentTypeError(f'Year {year} is out of the allowed range {HISTORICAL_START_YEAR}-{current_year}')
+
+    # handling res arg exception
     if args.res not in ['low', 'high']:
-        raise argparse.ArgumentTypeError("Resolution must be either 'low' or 'high'.")
+        raise argparse.ArgumentTypeError('Resolution must be either "low" or "high"')
+
+    # handling start and end args exceptions
+    year_out_of_range(args.start)
+    year_out_of_range(args.end)
+    if args.end < args.start:
+        raise argparse.ArgumentTypeError(
+            f'Ending year {args.end} must be later than {args.start}  is out of the allowed range {HISTORICAL_START_YEAR}-{current_year}')
+
+    # handling algorithm choice exception
+    if args.alg not in ['max_wind_speed', 'number_of_hits', 'median_wind_speed', 'degree_of_severity']:
+        raise argparse.ArgumentTypeError('Algorithm must be either "max_wind_speed", "number_of_hits", "median_wind_speed" or "degree_of_severity"')
 
     generateHeatMap(
-        res=args.res
+        start=args.start,
+        end=args.end,
+        alg=args.alg,
+        res=args.res,
     )
