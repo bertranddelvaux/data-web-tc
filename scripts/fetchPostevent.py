96ef78a0291d163b16cb246b7e44b38083e739c5
bertranddelvaux <80839599+bertranddelvaux@users.noreply.github.com>
2025-02-27 18:19:50 +0000

    Update fetchPostevent.py with impact_dir = os.path.join(root_root, 'impact_15as')

--- scripts/fetchPostevent.py
+++ scripts/fetchPostevent.py
@@ -28,6 +28,7 @@ list_subfolder = ['ofcl_15as_nc', 'taos_swio30s_ofcl_windwater_nc', 'taos_swio30
 root_root = os.path.abspath(os.getcwd())
 os.chdir('mpres_data/postevent')
 dir_root = os.path.abspath(os.getcwd())
+impact_dir = os.path.join(root_root, 'impact_15as')
 
 for subfolder, ext in zip(list_subfolder, ['.nc', '.nc', '.zip']):
 
@@ -86,7 +87,8 @@ for subfolder, ext in zip(list_subfolder, ['.nc', '.nc', '.zip']):
                             mapping_file=os.path.join(root_root, 'mapping_15as.gzip'),
                             split=False,
                             geojson=False,
-                            gadm_file=os.path.join(root_root, 'gadm_adm2.json')
+                            gadm_file=os.path.join(root_root, 'gadm_adm2.json'),
+                            impact_dir=impact_dir
                         )
                     except:
                         print('\033[91m' + 'There was an error in trying to handle a 15as case' + '\033[0m')
